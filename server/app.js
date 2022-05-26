7c1ee3a0f4b471d14120bffe076a7afb2adeb898
Valery <valer4ik-larik@mail.ru>
2022-05-26 15:01:14 +0300

    add db teamplate

---
+++ server/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.DB_PORT ?? 3001;
+
+app.get('/', (req, res) => {
+  res.send('Ololo');
+});
+
+app.listen(PORT, () => {
+  console.log('Let`s do it!');
+});
