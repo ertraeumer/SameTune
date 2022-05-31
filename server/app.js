159f351223c9edb5dcb68eafa765ba0203d58aa3
Andrey Pomogaev <andrey.pomogaev@gmail.com>
2022-05-31 17:28:31 +0300

    multer in process

--- server/app.js
+++ server/app.js
@@ -7,11 +7,13 @@ const app = express();
 const PORT = process.env.DB_PORT ?? 3001;
 const filterRouter = require('./routes/filterRouters');
 const bandsRouter = require('./routes/bandsRouters');
-const musiciansRouter = require('./routes/musiciansRouters')
+const musiciansRouter = require('./routes/musiciansRouters');
+const musicianPhotoRouter = require('./routes/musicianPhotoRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Olololo');
@@ -19,7 +21,8 @@ app.get('/', (req, res) => {
 
 app.use('/api/filter', filterRouter);
 app.use('/api/bands', bandsRouter);
-app.use('/api/musicians', musiciansRouter)
+app.use('/api/musicians', musiciansRouter);
+app.use('/api/userProfile/addphoto', musicianPhotoRouter);
 
 app.listen(PORT, () => {
   console.log('Let`s do it!');
