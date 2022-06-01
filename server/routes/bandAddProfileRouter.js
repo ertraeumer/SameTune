534b13fa02f08e972e82d09dc10bc679d138bccb
Andrey Pomogaev <andrey.pomogaev@gmail.com>
2022-06-01 10:39:30 +0300

    add group profile option added

--- server/routes/bandAddProfileRouter.js
+++ server/routes/bandAddProfileRouter.js
@@ -1,21 +1,33 @@
 const router = require('express').Router();
-const { Group } = require('../db/models');
+const {
+  Group, Genre, Location, User,
+} = require('../db/models');
 
 router.post('/', async (req, res) => {
   const {
     name, description, photo, genre, location, owner,
   } = req.body;
 
-  const newEntry = await Group.create({
-    name,
-    description,
-    photo,
-    genreId: genre,
-    locationId: location,
-    ownerId: owner,
-  });
-  console.log(newEntry);
-  res.status(200);
+  try {
+    const genreId = await Genre.findAll({ where: { name: genre } });
+    const locationId = await Location.findAll({ where: { name: location } });
+    const ownerId = await User.findAll({ where: { name: owner } });
+
+    console.log('ownerId --->', ownerId[0].dataValues.id);
+
+    await Group.create({
+      name,
+      description,
+      photo,
+      genreId: genreId[0].dataValues.id,
+      locationId: locationId[0].dataValues.id,
+      ownerId: ownerId[0].dataValues.id,
+    });
+    res.status(200).send('success');
+  } catch (error) {
+    res.status(500).end();
+    console.log(error);
+  }
 });
 
 module.exports = router;
