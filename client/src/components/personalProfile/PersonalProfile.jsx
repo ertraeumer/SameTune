d2ed5ee23f21d08168e330973132199236af60e9
alextsyg <innempaya@gmail.com>
2022-06-02 19:14:12 +0300

    Profile page styles_2

--- client/src/components/personalProfile/PersonalProfile.jsx
+++ client/src/components/personalProfile/PersonalProfile.jsx
@@ -59,7 +59,6 @@ const PersonalProfile = () => {
           <ListGroup >
             <ListGroup.Item style={{ paddingTop: 0, paddingBottom: 0 }}>
               <div className={styles.nameBox}>
-                <div>
                 <div className={styles.prop}><b>Name:</b></div>
                 <div className={styles.propInfo}>
                   {editableName ?
@@ -69,13 +68,11 @@ const PersonalProfile = () => {
                       authUser?.name || 'No Information Provided'
                     )}
                 </div>
-                </div>
                 <div className={styles.edit} onClick={changeNameHandler}>{editableName ? 'save' : 'edit'}</div>
               </div>
             </ListGroup.Item>
             <ListGroup.Item style={{ paddingTop: 0, paddingBottom: 0 }}>
               <div className={styles.nameBox}>
-                <div>
                   <div className={styles.prop}><b>Location:</b></div>
                   <div className={styles.propInfo}>
                     {editableLocation ?
@@ -92,19 +89,16 @@ const PersonalProfile = () => {
                       <div className={styles.content}>{authUser?.location || 'No Information Provided'}</div>
                     )}
                   </div>
-                </div>
                 <div className={styles.edit} onClick={changeLocationHandler}>{editableLocation ? 'save' : 'edit'}</div>
               </div>
             </ListGroup.Item>
             <ListGroup.Item style={{ paddingTop: 0, paddingBottom: 0 }}>
               <div className={styles.nameBox}>
-                <div>
-                <div className={styles.prop}><b>Instruments I Play:</b></div>
-                <div style={{ width: '100%' }}>
-                  <ul className={styles.propInfo} style={{ marginRight: '3rem', listStyleType: 'none', marginLeft: '0px'}}>
-                    {authUser?.instruments ? authUser?.instruments.map((el) => <li key={Math.trunc(Math.random() * 1000)}>{el}</li>) : 'No information provided'}
+                  <div className={styles.prop}><b>Instruments I Play:</b></div>
+                  <div className={styles.propInfo}>
+                    {authUser?.instruments ? authUser?.instruments.map((el, i) => ((authUser.instruments.length - 1) === i ? (<span key={Math.trunc(Math.random() * 1000)}>{el}</span>) : (<span key={Math.trunc(Math.random() * 1000)}>{el}, </span>))) : 'No Information Provided'}
                     {editableInstruments ? (
-                      <li>
+                      <span>
                         { instrumentToAdd ? (
                           <select style={{ width: '7.6rem', marginRight: 0 }} value={instrumentToAdd} onChange={(e) => setInstrumentToAdd(e.target.value)}>
                             <option value='' disabled hidden>Выбор</option>
@@ -120,48 +114,38 @@ const PersonalProfile = () => {
                             })}
                           </select>
                         )}
-                      </li>
+                      </span>
                     ) : (<div></div>)}
-                  </ul>
                   </div>
-                </div>
                 <div className={styles.edit} onClick={() => addInstrumentHandler(instrumentToAdd)}>add</div>
               </div>
             </ListGroup.Item>
             <ListGroup.Item style={{ paddingTop: 0, paddingBottom: 0 }}>
               <div className={styles.nameBox}>
-                <div>
                   <div className={styles.prop}><b>Preferable Genres:</b></div>
                   <div className={styles.propInfo}>{authUser?.genres ? authUser.genres.join(', ') : 'No Information Provided'}</div>
-                </div>
-                <div className={styles.edit} onClick={() => console.log('test')}>add</div>
-              </div> 
+                  <div className={styles.edit} onClick={() => console.log('test')}>add</div>
+              </div>
             </ListGroup.Item>
             <ListGroup.Item style={{ paddingTop: 0, paddingBottom: 0 }}>
               <div className={styles.nameBox}>
-                <div>
                   <div className={styles.prop}><b>About Me:</b></div>
                   <div className={styles.propInfo}>{authUser?.profile || 'No Information Provided'}</div>
-                </div>
-                <div className={styles.edit}>edit</div>
+                  <div className={styles.edit}>edit</div>
               </div>
             </ListGroup.Item>
             <ListGroup.Item style={{ paddingTop: 0, paddingBottom: 0 }}>
               <div className={styles.nameBox}>
-                <div>
                   <div className={styles.prop}><b>My Bands:</b></div>
-                  <div className={styles.propInfo}>{authUser?.groups ? authUser.groups.join(', ') : 'No Information Provided'}</div>      
-                </div>
-                <div className={styles.edit}></div>
+                  <div className={styles.propInfo}>{authUser?.groups || 'No Information Provided'}</div>      
+                  <div className={styles.edit}></div>
               </div>
             </ListGroup.Item>
             <ListGroup.Item style={{ paddingTop: 0, paddingBottom: 0 }}>
               <div className={styles.nameBox}>
-                <div>
                   <div className={styles.prop}><b>Contacts:</b></div>
                   <div className={styles.propInfo}>{authUser?.email} {authUser?.phone}</div> 
-                </div>
-                <div className={styles.edit}></div>
+                  <div className={styles.edit}></div>
               </div>
             </ListGroup.Item>
           </ListGroup>
